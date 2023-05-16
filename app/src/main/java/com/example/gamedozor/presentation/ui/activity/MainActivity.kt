6a21da956ae30516adc870949ccf9bb61eb02fd9
Artem <artem.sivokoz@gmail.com>
2023-05-16 18:19:14 +0300

    Creating project

---
+++ app/src/main/java/com/example/gamedozor/presentation/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gamedozor.presentation.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gamedozor.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
