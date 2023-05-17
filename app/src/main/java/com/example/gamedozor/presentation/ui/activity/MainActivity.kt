e96396d63dd7bc1a7b15220ec9ca933b622f4bbd
Artem <artem.sivokoz@gmail.com>
2023-05-17 23:31:14 +0300

    #2 Create destinations and bottom navigation bar

--- app/src/main/java/com/example/gamedozor/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/gamedozor/presentation/ui/activity/MainActivity.kt
@@ -2,17 +2,51 @@ package com.example.gamedozor.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.gamedozor.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navMainHostActivity) as NavHostFragment
         navController = navHostFragment.navController
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationBar)
+
+        bottomNavigationView.visibility = View.GONE
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.profile_menu -> {
+                    navController.setGraph(R.navigation.nav_profile_graph)
+                    true
+                }
+                R.id.available_games_menu -> {
+                    navController.setGraph(R.navigation.nav_available_games_graph)
+                    true
+                }
+                R.id.chats_menu -> {
+                    navController.setGraph(R.navigation.nav_chats_graph)
+                    true
+                }
+                R.id.my_games_menu -> {
+                    navController.setGraph(R.navigation.nav_my_games_graph)
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/gamedozor/presentation/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/gamedozor/presentation/ui/fragments/LoginFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.gamedozor.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class LoginFragment : Fragment() {
 
     private lateinit var buttonToRegistrationFragment: TextView
+    private lateinit var buttonLogin: Button
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
@@ -20,12 +23,18 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         buttonToRegistrationFragment = view.findViewById(R.id.goToRegistrationText)
+        buttonLogin = view.findViewById(R.id.signInAccountButton)
         setClickToButtons()
     }
 
     private fun setClickToButtons() {
         buttonToRegistrationFragment.setOnClickListener {
             findNavController().navigate(R.id.registrationFragment)
+        }
+        buttonLogin.setOnClickListener {
+            findNavController().setGraph(R.navigation.nav_profile_graph)
+            val bottomNavigationView = requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavigationBar)
+            bottomNavigationView.visibility = View.VISIBLE
         }
     }
 
