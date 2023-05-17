84c4338f92388245931065ef02beaa1c9e46b801
Artem <artem.sivokoz@gmail.com>
2023-05-17 22:16:21 +0300

    #1 Switch between registration and login forms

--- app/src/main/java/com/example/gamedozor/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/gamedozor/presentation/ui/activity/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.gamedozor.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.gamedozor.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navMainHostActivity) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
--- app/src/main/java/com/example/gamedozor/presentation/ui/fragments/RegistrationFragment.kt
+++ app/src/main/java/com/example/gamedozor/presentation/ui/fragments/RegistrationFragment.kt
@@ -1,13 +1,18 @@
-package com.example.gamedozor
+package com.example.gamedozor.presentation.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.example.gamedozor.R
 
 
 class RegistrationFragment : Fragment() {
+
+    private lateinit var buttonToLoginFragment: TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_registration, container, false)
@@ -15,6 +20,14 @@ class RegistrationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        buttonToLoginFragment = view.findViewById(R.id.goToLoginText)
+        setClickToButtons()
+    }
+
+    private fun setClickToButtons() {
+        buttonToLoginFragment.setOnClickListener {
+            findNavController().navigate(R.id.loginFragment)
+        }
     }
 
 }
