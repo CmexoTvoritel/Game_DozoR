776548137e45603a0dabf7d43059df2144a22ff6
Artem <artem.sivokoz@gmail.com>
2023-06-27 22:13:33 +0300

    #9 Done my games View

---
+++ app/src/main/java/com/example/gamedozor/presentation/models/MyGamesModel.kt
@@ -0,0 +1,9 @@
+package com.example.gamedozor.presentation.models
+
+data class MyGamesModel (
+        val nameGame: String,
+        val currentTeams: Int,
+        val maximumTeams: Int,
+        val dataGame: String,
+        val timeGame: String,
+)
---
+++ app/src/main/java/com/example/gamedozor/presentation/ui/viewholders/MyGamesViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.gamedozor.presentation.ui.viewholders
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gamedozor.databinding.ItemMyGameLayoutBinding
+
+class MyGamesViewHolder(val binding: ItemMyGameLayoutBinding): RecyclerView.ViewHolder(binding.root) {
+}
